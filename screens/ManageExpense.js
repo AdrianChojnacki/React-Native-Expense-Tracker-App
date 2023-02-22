43e00c30807a6d21bfbc726757c02c62ef8fd3c5
Adrian Chojnacki <adrian.chojnacki@frontkom.com>
2023-02-22 15:45:57 +0100

    Added some attributes to Inputs

--- screens/ManageExpense.js
+++ screens/ManageExpense.js
@@ -1,5 +1,6 @@
 import { useContext, useLayoutEffect } from "react";
-import { View, StyleSheet, TextInput } from "react-native";
+import { View, StyleSheet } from "react-native";
+import ExpenseForm from "../components/ManageExpense/ExpenseForm";
 import Button from "../components/UI/Button";
 import IconButton from "../components/UI/IconButton";
 import { GlobalStyles } from "../constants/styles";
@@ -45,7 +46,7 @@ function ManageExpense({ route, navigation }) {
 
   return (
     <View style={styles.container}>
-      <TextInput />
+      <ExpenseForm />
       <View style={styles.buttons}>
         <Button style={styles.button} onPress={confirmHandler}>
           {isEditing ? "Update" : "Add"}
